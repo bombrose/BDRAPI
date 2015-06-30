c305e62a2deb2a7eadbeb40a3a22b97dc46860e1
bombrose <bombrose@gmail.com>
2015-06-30 14:41:45 +0800

    Update BDRApi.py

--- BDRApi/BDRApi.py
+++ BDRApi/BDRApi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Python 百度即用api sdk(http://apistore.baidu.com/apiworks/readyapiprefecture.html).
+Python 百度即用api sdk(http://apistore.baidu.com).
 '''
 __version__ = '0.1.0'
 __author__ = 'czc(chenzhch@gmail.com)'
